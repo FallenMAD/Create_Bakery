37ed57eb0436b4afda8eb25951caac9a8dabef4d
Roman Kvas <fallenmadara@192.168.1.11>
2024-05-20 08:33:09 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,7 @@
+"use strict";
+window.addEventListener("hashchange", ()=>{
+    if (window.location.hash === "#menu") document.body.classList.add("page__body--with-menu");
+    else document.body.classList.remove("page__body--with-menu");
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
